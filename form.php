7f8945b56e1424106658fbf74efdd8777db03f2a
ypoudel3 <yunishapoudel6@gmail.com>
2025-02-01 19:36:29 +0545

    Assignment

---
+++ form.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="contact.php" method="GET">
+    <fieldset>
+    <legend>User Login:</legend>
+    <br>
+    Username:<br>
+    <input type="text" name="username" placeholder="username">
+    <br>
+    Password:<br>
+    <input type="password" name="password" placeholder="password">
+    <br><br>
+    <input type="submit" name="submit">
+</fieldset>
+</form>
+</body>
+</html>
